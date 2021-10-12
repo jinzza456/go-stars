eb67f1257cbec39af37a22812d6a1aa3ffd7bcd2
jinzza456 <jinzza456@gmail.com>
2021-10-12 15:33:24 +0900

    diamond

--- stars.go
+++ stars.go
@@ -4,17 +4,22 @@ import "fmt"
 
 func main() {
 
-	for i := 0; i < 3; i++ {
-		for j := 0; j < i+1; j++ {
+	for i := 1; i < 5; i++ {
+		for j := 1; j < 5-i; j++ {
+			fmt.Print(" ")
+		}
+		for x := 1; x <= (i*2 - 1); x++ {
 			fmt.Print("*")
 		}
-		fmt.Println()
+		println("")
 	}
-
-	for i := 0; i < 2; i++ {
-		for j := 0; j < 2-i; j++ {
+	for i := 1; i < 4; i++ {
+		for j := 1; j < i+1; j++ {
+			fmt.Print(" ")
+		}
+		for x := 1; x <= (7 - i*2); x++ {
 			fmt.Print("*")
 		}
-		fmt.Println()
+		println("")
 	}
 }
