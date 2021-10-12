1aa2c0587202c9402608977c381d272ae793ae96
jinzza456 <jinzza456@gmail.com>
2021-10-12 14:13:32 +0900

    12321 stars

--- stars.go
+++ stars.go
@@ -4,15 +4,15 @@ import "fmt"
 
 func main() {
 
-	// for i := 0; i < 5; i++ {
-	// 	for j := 0; j < i+1; j++ {
-	// 		fmt.Print("*")
-	// 	}
-	// 	fmt.Println()
-	// }
+	for i := 0; i < 3; i++ {
+		for j := 0; j < i+1; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
 
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5-i; j++ {
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2-i; j++ {
 			fmt.Print("*")
 		}
 		fmt.Println()
